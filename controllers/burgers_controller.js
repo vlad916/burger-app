587a7c39d775dfc8e12baf364ced4435494171c0
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-26 18:08:49 +0930

    controllers and models fixed.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,36 +1,39 @@
-const express = require ('express');
-const burger = require ('../models/burger.js');
+
+const express = require("express");
+const burger = require("../models/burger.js");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    burger.selectAll('burgers', (data) => {
-        const burgerDataObj = {
-              burgers: data
-        };
-    });
+router.get("/", (req, res) => {
+	burger.selectAll("burgers", (data) => {
+		let burgerDataObj = {
+			burgers: data
+		};
+		console.log(burgerDataObj);
+		res.render("index", burgerDataObj);
+	});
 });
 
-router.post('/api/burgers', (req, res) => {
-    burger.insertOne ('burger_name', [req.body.burger], (err, result) => {
-        if (err) {
-            console.log(err)
-        }
-        console.log ('Post Route...')
-        res.redirect('/');
-    });
+router.post("/api/burgers", (req, res) => {
+	burger.insertOne("burger_name", [req.body.burger], function(err, result) {
+		if (err) {
+			console.log(err)
+		}
+		console.log("hitting post route")
+		res.redirect("/");
+	});
 });
 
-router.put('/api/burgers/:id', (req, res) => {
-    const condition = 'id = ' + req.params.id;
-    console.log('condition', condition);
-    console.log(req.params.id);
-    burger.updateOne({devoured: req.body.devoured}, condition, (result) => {
-        if (result.changedRows == 0) {
-            return res.status (404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+router.put("/api/burgers/:id", (req, res) => {
+	let condition = "id = " + req.params.id;
+	console.log("condition", condition);
+	console.log(req.params.id);
+	burger.updateOne({devoured: req.body.devoured}, condition, (result) => {
+		if(result.changedRows == 0) {
+			return res.status(404).end();
+		} else {
+			res.status(200).end();
+		}
+	});
 });
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,24 @@
-const orm = require ('../config/orm.js');
 
-const burger = {
-    selectAll: function (table, cb) {
-        orm.selectAll ('burgers', (res) => {
-            cb (res);
-        });
-    },
-    insertOne: function (colName, valOfCol, cb) {
-        orm.insertOne('burgers', colName, valOfCol, (res) => {
-            cb (res);
-        });
-    },
-    updateOne: function (valOfCol, condition, cb) {
-        orm.updateOne('burgers', valOfCol, condition, (res) => {
-            cb (res);
-        });
-    }
+const orm = require("../config/orm.js");
+
+
+const burger = { 
+	// Select everything from the burgers table
+	selectAll: function(table, cb) {
+		orm.selectAll("burgers", function(res) {
+			cb(res);
+		});
+	},
+	insertOne: function(colName, valOfCol, cb) {
+		orm.insertOne("burgers", colName, valOfCol, (res) => {
+			cb(res);
+		});
+	},
+	updateOne: function(valOfCol, condition, cb) { 
+		orm.updateOne("burgers", valOfCol, condition, (res) => {
+			cb(res);
+		});
+	}
 };
 
 module.exports = burger;
