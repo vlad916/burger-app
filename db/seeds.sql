fe5dc53b944fef3c3581bfc2ed22494eaf42b38a
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-26 16:38:01 +0930

    Seeds.sql added.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers ( burger_name ) VALUES ("Beef Burger");
+INSERT INTO burgers ( burger_name ) VALUES ("Chicken Burger");
+INSERT INTO burgers ( burger_name ) VALUES ("Chesse Burger");
