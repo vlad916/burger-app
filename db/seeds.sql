1b297ee3f08e313fa4287ad8938adcf8d59a9e24
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-27 18:23:08 +0930

    Fixed seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers ( burger_name ) VALUES ("Beef Burger");
+INSERT INTO burgers ( burger_name ) VALUES ("Buffalo Burger");
 INSERT INTO burgers ( burger_name ) VALUES ("Chicken Burger");
 INSERT INTO burgers ( burger_name ) VALUES ("Cheese Burger");
