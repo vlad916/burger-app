d540a1abf24b37eb9523cdb8b166772d3be72f81
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-28 11:22:34 +0930

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,27 @@
-const mysql = require ('mysql');
+const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "pass12345",
     database: "burgers_db"
   });
-  
+}
+
   // Make connection.
-  connection.connect(function(err) {
+  connection.connect(function (err) {
     if (err) {
       console.error("error connecting: " + err.stack);
       return;
     }
     console.log("connected as id " + connection.threadId);
   });
-  
+
   // Export connection for our ORM to use.
   module.exports = connection;
-  
